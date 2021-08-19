6104989c102ec836812bb37961b8bb034c6e826e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-08-19 14:16:33 +0300

    style fix more

--- static/master.js
+++ static/master.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-
-
   $(".inputAreas2").click(function(){
     $(".dropdown-content").slideToggle(250);
   });
 
-
-
+  if($( window ).width() < 960){
+      $("#buttonSpace").text("");
+  }
 });
